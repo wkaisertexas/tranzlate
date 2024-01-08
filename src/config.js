85911944156c62cb5398f7b969fd94a492136175
William Kaiser <wkaisertexas@gmail.com>
2024-01-08 14:50:06 -0600

    added proper config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,9 @@
-/// Handles the configuration of the application
-
-const supportedTranslations = ["en", "es", "fr", "de", "it", "pt", "ru", "zh"];
+import { SUPPORTED_TRANSLATIONS } from "./consts.js";
 
 const conditionallySetLanguage = () => {
   if (process.argv.length < 3) return;
   if (process.argv[0] !== "set-language") return;
-  if (!supportedTranslations.includes(process.argv[2])) return;
+  if (!SUPPORTED_TRANSLATIONS.includes(process.argv[2])) return;
 
   localStorage.setItem("language", process.argv[2]);
   process.exit(0);
