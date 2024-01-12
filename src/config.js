87abeb1e7729e3b7d3d15100ffea1a20cc67b24a
William Kaiser <wkaisertexas@gmail.com>
2024-01-11 22:51:37 -0800

    moved config TUI to separate page to resolve dependency issues

--- src/config.js
+++ src/config.js
@@ -1,13 +1,10 @@
-import { SUPPORTED_TRANSLATIONS, BASE_LANGUAGE, LANGUAGES } from "./consts.js";
-import { gracefulExit, matchSupportedTranslations } from "./helpers.js";
+import { BASE_LANGUAGE } from "./consts.js";
 
 import { readFileSync, writeFileSync } from "fs";
-import { intro, select, outro } from "@clack/prompts";
 
 import path from "path";
 import os from "os";
 import ini from "ini";
-import colors from "picocolors";
 
 const CONFIG_PATH = path.join(os.homedir(), ".tranzlate");
 
@@ -20,28 +17,9 @@ const config = (() => {
 })();
 
 // Setters
-const conditionallySetLanguage = async () => {
-  console.log(process.argv);
-  if (process.argv.length < 3) return;
-  if (process.argv[3] !== "set-language" && process.argv[2] !== "set-language")
-    return;
-
-  console.clear();
-
-  let language_to_set = process.argv[3];
-  if (matchSupportedTranslations(process.argv[2])) {
-    language_to_set = process.argv[2];
-  }
-  if (!matchSupportedTranslations(language_to_set)) {
-    language_to_set = await askUserForLanguage();
-  }
-
-  config.language = language_to_set;
+const setLanguage = (language) => {
+  config.language = language;
   saveConfig();
-
-  outro(convertString(`Language set to ${LANGUAGES[language_to_set]}`));
-
-  process.exit(0);
 };
 
 const setAPIKey = (key) => {
@@ -68,36 +46,4 @@ const getAPIKeyFromConfig = () => {
 
   return;
 };
-
-import { convertString } from "./localization.js";
-
-// Helpers
-const askUserForLanguage = async () => {
-  intro(
-    `${colors.bgCyan(
-      colors.black(convertString(" tranzlate: default language selection")),
-    )}`,
-  );
-
-  let language = await select({
-    message: convertString("Select a language to set as your default language"),
-    options: SUPPORTED_TRANSLATIONS.map((language) => {
-      return {
-        label: `${language} - ${LANGUAGES[language]}`,
-        value: language,
-      };
-    }),
-    required: true,
-  });
-
-  gracefulExit(language);
-
-  return language;
-};
-
-export {
-  conditionallySetLanguage,
-  getLanguage,
-  getAPIKeyFromConfig,
-  setAPIKey,
-};
+export { getLanguage, getAPIKeyFromConfig, setAPIKey, setLanguage };
---
+++ src/configTUI.js
@@ -0,0 +1,57 @@
+import { LANGUAGES, SUPPORTED_TRANSLATIONS } from "./consts.js";
+import { gracefulExit, matchSupportedTranslations } from "./helpers.js";
+
+import { setLanguage } from "./config.js";
+
+import { convertString } from "./localization.js";
+import { intro, select, outro } from "@clack/prompts";
+
+import colors from "picocolors";
+
+const conditionallySetLanguage = async () => {
+  if (process.argv.length < 3) return;
+  if (process.argv[3] !== "set-language" && process.argv[2] !== "set-language")
+    return;
+
+  console.clear();
+
+  let language_to_set = process.argv[3];
+  if (matchSupportedTranslations(process.argv[2])) {
+    language_to_set = process.argv[2];
+  }
+  if (!matchSupportedTranslations(language_to_set)) {
+    language_to_set = await askUserForLanguage();
+  }
+
+  setLanguage(language_to_set);
+
+  outro(convertString(`Language set to ${LANGUAGES[language_to_set]}`));
+
+  process.exit(0);
+};
+
+// Helpers
+const askUserForLanguage = async () => {
+  intro(
+    `${colors.bgCyan(
+      colors.black(convertString(" tranzlate: default language selection")),
+    )}`,
+  );
+
+  let language = await select({
+    message: convertString("Select a language to set as your default language"),
+    options: SUPPORTED_TRANSLATIONS.map((language) => {
+      return {
+        label: `${language} - ${LANGUAGES[language]}`,
+        value: language,
+      };
+    }),
+    required: true,
+  });
+
+  gracefulExit(language);
+
+  return language;
+};
+
+export { conditionallySetLanguage };
